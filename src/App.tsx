74e7c86a95288a82412ee450d803c4b02c42a8b6
Shreya Choure <choureshreya295@gmail.com>
2024-02-12 13:41:51 -0800

    Built a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import { Spinner } from "@chakra-ui/react";
-import "./App.css";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
 	return (
 		<>
-			<Spinner color="red.500" />
-			<p>Website under construction</p>
+			<Grid
+				templateAreas={{
+					base: `"header" "main" "footer"`,
+					lg: `"header header" "aside main" "aside footer"`,
+				}}
+			>
+				<GridItem bg="orange.300" area={"header"}>
+					Header
+				</GridItem>
+				<Show above="lg">
+					<GridItem bg="pink.300" area={"aside"}>
+						Aside
+					</GridItem>
+				</Show>
+				<GridItem bg="green.300" area={"main"}>
+					Main
+				</GridItem>
+				<GridItem bg="blue.300" area={"footer"}>
+					Footer
+				</GridItem>
+			</Grid>
 		</>
 	);
 }
