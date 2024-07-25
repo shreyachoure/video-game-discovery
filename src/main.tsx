79cf6258d5c90b613011d638b1e1e4a976f35a52
Shreya Choure <choureshreya295@gmail.com>
2024-07-25 11:26:12 -0700

    removed next component

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App.tsx";
 import theme from "./theme.ts";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+
 
 const queryClient = new QueryClient();
 
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
 			<ColorModeScript initialColorMode={theme.config.initialColorMode} />
 			<QueryClientProvider client={queryClient}>
 				<App />
-				<SpeedInsights />
+				
 			</QueryClientProvider>
 		</ChakraProvider>
 	</React.StrictMode>
